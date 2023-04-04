36e81b2b248a442f9dd64e9205a324888113e212
Aniket Hirpara <gdg4dev@gmail.com>
2023-04-04 13:36:13 +0530

    ehterscan api connected

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const axios = require('axios')
+
+app.get('/token/:tokenAddr/user/:userAddr/amount/:tokenAmount', (req,res) => {
+    const response = {
+        error: false,
+        result: false
+    }
+    const {tokenAddr, userAddr, tokenAmount} = req.params
+
+    axios.get(`https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${tokenAddr}&address=${userAddr}&tag=latest&apikey=${process.env.ETHERSCAN_API_KEY}`)
+    .then((d) => {
+        if (d.data.result/1000000000000000000 >= tokenAmount){
+            response.result =  true
+            return res.json(response)
+        }
+        return res.json(response)
+    }).catch((e) => {
+        response.error = true
+        return res.json(response)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`)
+})
